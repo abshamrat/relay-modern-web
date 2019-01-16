cc8cf13c2b1233b639dc0985fb1cc189bdc35054
shamrat17 <shamratakbar17@gmail.com>
2019-01-16 19:11:20 +0600

    changed the layout height

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,52 +15,38 @@ class Home extends Component {
     }
     render() {
         return (
-            <Layout>
-                <Header className="header">
+            <Layout style={{height: "100vh"}}>
+                <Header className="header" style={{ paddingLeft: "10%", paddingRight: "10%" }}>
                     <div className="logo" />
                     <Menu
                         theme="dark"
                         mode="horizontal"
-                        defaultSelectedKeys={['2']}
+                        defaultSelectedKeys={['1']}
                         style={{ lineHeight: '64px' }}
                     >
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
+                        <Menu.Item key="1">Home</Menu.Item>
+                        <Menu.Item key="2">About</Menu.Item>
+                        <Menu.Item key="3">Contact</Menu.Item>
                     </Menu>
                 </Header>
-                <Content style={{ padding: '0 50px' }}>
+                <Content style={{ paddingLeft: "10%", paddingRight: "10%" }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                         <Breadcrumb.Item>App</Breadcrumb.Item>
                     </Breadcrumb>
-                    <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                    <Layout style={{ padding: '24px 0', background: '#fff', height: "80vh"}}>
                         <Sider width={200} style={{ background: '#fff' }}>
                             <Menu
                                 mode="inline"
                                 defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
+                                defaultOpenKeys={['side']}
                                 style={{ height: '100%' }}
                             >
-                                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                                    <Menu.Item key="5">option5</Menu.Item>
-                                    <Menu.Item key="6">option6</Menu.Item>
-                                    <Menu.Item key="7">option7</Menu.Item>
-                                    <Menu.Item key="8">option8</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                                    <Menu.Item key="9">option9</Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
-                                </SubMenu>
+                                <Menu.Item key="1">Create</Menu.Item>
+                                <Menu.Item key="2">Read</Menu.Item>
+                                <Menu.Item key="3">Update</Menu.Item>
+                                <Menu.Item key="4">Delete</Menu.Item>
                             </Menu>
                         </Sider>
                         <Content style={{ padding: '0 24px', minHeight: 280 }}>
@@ -68,9 +54,9 @@ class Home extends Component {
                         </Content>
                     </Layout>
                 </Content>
-                    <Footer style={{ textAlign: 'center' }}>
-                        Ant Design ©2018 Created by Ant UED
-                    </Footer>
+                <Footer style={{ textAlign: 'center' }}>
+                    Practice
+                </Footer>
             </Layout>
         )
     }
